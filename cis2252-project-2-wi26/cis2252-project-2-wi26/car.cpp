f33b76cee495e1ac5a4ab218441a0d2460317145
anthonylang06-ui <lang.anthony@student.oaklandcc.edu>
2026-04-06 22:48:06 -0400

    start of car.cpp

--- cis2252-project-2-wi26/cis2252-project-2-wi26/car.cpp
+++ cis2252-project-2-wi26/cis2252-project-2-wi26/car.cpp
@@ -1,2 +1,13 @@
+//lets me use cout
 #include <iostream>
+//car class is now included (duh)
 #include "Car.h"
+//constructor, sets year make + model
+//sets speed to start at 0
+Car::Car(int y, string mk, string mdl)
+{
+    year = y;
+    make = mk;
+    model = mdl;
+    speed = 0;
+}
