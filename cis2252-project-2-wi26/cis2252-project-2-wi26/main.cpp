40b06402da100664aae9548869804bc163d68d1a
anthonylang06-ui <lang.anthony@student.oaklandcc.edu>
2026-04-07 01:52:44 -0400

    Finished and tested code

--- cis2252-project-2-wi26/cis2252-project-2-wi26/main.cpp
+++ cis2252-project-2-wi26/cis2252-project-2-wi26/main.cpp
@@ -14,7 +14,7 @@ int main()
     cout << "Car: " << car1.getYear() << " " << car1.getMake() << " " << car1.getModel() << endl;
     cout << "Speed: " << car1.getSpeed() << endl;
     cout << endl;
-    
+    //accelerates 5 times
     cout << "Accelerate 5 times" << endl;
     for (int i = 0; i < 5; i++)
     {
@@ -22,4 +22,28 @@ int main()
         cout << "Speed: " << car1.getSpeed() << endl;
     }
     cout << endl;
-}
+    //Brake 5 times
+    cout << "Brake 5 times" << endl;
+      for (int i = 0; i < 5; i++)
+      {
+          car1.brake();
+          cout << "Speed: " << car1.getSpeed() << endl;
+      }
+
+      cout << endl;
+//test brakes at 0
+      cout << "Test brake at 0" << endl;
+      car1.brake();
+      cout << "Speed: " << car1.getSpeed() << endl;
+
+      cout << endl;
+//test acceleration past 150
+      cout << "Test accelerate past 150" << endl;
+      for (int i = 0; i < 31; i++)
+      {
+          car1.accelerate();
+          cout << "Speed: " << car1.getSpeed() << endl;
+      }
+
+      return 0;
+  }
